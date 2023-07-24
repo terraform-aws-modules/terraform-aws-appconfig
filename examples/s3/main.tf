211dee06a00ee012550ebba9a415cf7633747505
Matthieu Lienart <matthieu.lienart@amanox.ch>
2023-07-24 18:05:34 +0200

    Bump S3 bucket module version for terraform provider version constraints

--- examples/s3/main.tf
+++ examples/s3/main.tf
@@ -98,7 +98,7 @@ module "validate_lambda" {
 
 module "s3_bucket" {
   source  = "terraform-aws-modules/s3-bucket/aws"
-  version = "~> 2.0"
+  version = "~> 3.0"
 
   bucket = "${local.name}-${data.aws_caller_identity.current.account_id}-${data.aws_region.current.name}"
   acl    = "private"
